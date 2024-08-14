e4f12534ba20b30c0b0ea48b35232063f8957ab7
rayhanatuljannah <105841110822@student.unismuh.ac.id>
2024-08-14 10:04:05 +0700

    Selesai Chapter 4

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "@/app/ui/global.css";
-import { lusitana } from "./ui/fonts";
+import { lusitana } from "@/app/ui/fonts";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
