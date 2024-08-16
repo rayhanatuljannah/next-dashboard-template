834a06f85c2909a693171f4e94d09db4b037246e
rayhanatuljannah <105841110822@student.unismuh.ac.id>
2024-08-16 23:51:57 +0700

    Selesai Chapter 9

--- app/lib/data.js
+++ app/lib/data.js
@@ -11,7 +11,7 @@ export async function fetchRevenue() {
 
     const data = await sql`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
